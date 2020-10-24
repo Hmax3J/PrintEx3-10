7653dd54d440c01b042f202237534d761c72a57e
Hmax3J <Hmax3J@gamil.com>
2020-10-24 11:27:43 +0900

    10

---
+++ PrintEx10/src/printex10/PrintEx10.java
@@ -0,0 +1,20 @@
+package printex10;
+import java.util.*;
+
+public class PrintEx10 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		System.out.println(a*(b%10));
+		System.out.println(a*(b%100/10));
+		System.out.println(a*(b/100));
+		System.out.println(a*b);
+
+	}
+
+}
