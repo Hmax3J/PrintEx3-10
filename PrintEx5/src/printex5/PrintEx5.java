e552734656b53a7709cc91924adc0d4678f6b110
Hmax3J <Hmax3J@gamil.com>
2020-10-24 11:25:46 +0900

    5

---
+++ PrintEx5/src/printex5/PrintEx5.java
@@ -0,0 +1,18 @@
+package printex5;
+import java.util.Scanner;
+
+public class PrintEx5 {
+
+	public static void main(String[] args) {
+		
+		Scanner plus = new Scanner(System.in);
+		
+		int a = plus.nextInt();
+		int b = plus.nextInt();
+		
+		int result = a-b;
+		System.out.println(result);		
+		
+	}
+
+}
