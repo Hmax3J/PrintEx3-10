e4c1808b61389454cb35a495653d7ef194b700f2
Hmax3J <Hmax3J@gamil.com>
2020-10-24 11:25:07 +0900

    4

---
+++ PrintEx4/src/printex4/PrintEx4.java
@@ -0,0 +1,18 @@
+package printex4;
+import java.util.*;
+
+public class PrintEx4 {
+
+	public static void main(String[] args) {
+		
+		Scanner plus = new Scanner(System.in);
+		
+		int a = plus.nextInt();
+		int b = plus.nextInt();
+		
+		int result = a+b;
+		System.out.println(result);		
+
+	}
+
+}
