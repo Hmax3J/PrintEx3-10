acbff20a4d6b3e3ba3e35b264f43db3d69314f52
Hmax3J <Hmax3J@gamil.com>
2020-10-24 11:26:10 +0900

    6

---
+++ PrintEx6/src/printex6/PrintEx6.java
@@ -0,0 +1,18 @@
+package printex6;
+import java.util.*;
+
+public class PrintEx6 {
+
+	public static void main(String[] args) {
+		
+		Scanner plus = new Scanner(System.in);
+		
+		int a = plus.nextInt();
+		int b = plus.nextInt();
+		
+		int result = a*b;
+		System.out.println(result);		
+
+	}
+
+}
