ccbd2f50ddc383d7d12e965569a9a633d66a112a
Hmax3J <Hmax3J@gamil.com>
2020-10-24 11:23:11 +0900

    3

---
+++ PrintEx3/src/printex3/PrintEx3.java
@@ -0,0 +1,15 @@
+package printex3;
+
+public class PrintEx3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+	}
+
+}
