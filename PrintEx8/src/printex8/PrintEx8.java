aa7b74c4abec677e3786fafa4e998a72d08f8146
Hmax3J <Hmax3J@gamil.com>
2020-10-24 11:27:01 +0900

    8

---
+++ PrintEx8/src/printex8/PrintEx8.java
@@ -0,0 +1,21 @@
+package printex8;
+import java.util.Scanner;
+
+public class PrintEx8 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+	}
+
+}
