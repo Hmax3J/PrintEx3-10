a4e89cd470a8c9be397963d380be5e10f5c5f3d9
Hmax3J <Hmax3J@gamil.com>
2020-10-24 11:26:34 +0900

    7

---
+++ PrintEx7/src/printex7/PrintEx7.java
@@ -0,0 +1,17 @@
+package printex7;
+import java.util.*;
+
+public class PrintEx7 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		double a = in.nextDouble();
+		double b = in.nextDouble();
+		
+		System.out.print(a/b);
+
+	}
+
+}
